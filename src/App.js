f19283522c3cd4f93d504245256e7e514bf6c605
Michal.Twarowski <Michal.Twarowski@UK00342>
2018-02-02 16:40:24 +0000

    Login With Firebase. Mess but works

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { auth, provider } from './firebase.js';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      user: null,
       practices: [{
-      Id: 1,
-      Name: 'richmondPractice',
-      Comment: 'Elbow to low',
-      TotalValue: 200,
-      PracticeDateTimeStamp: 500000
-    },{
-      Id: 2,
-      Name: 'LAPractice',
-      Comment: 'Ancker to low',
-      TotalValue: 150,
-      PracticeDateTimeStamp: 400000
+      id: 1,
+      name: 'richmondPractice',
+      comment: 'Elbow to low',
+      totalValue: 200,
+      practiceDateTimeStamp: 500000
     }]};
+
+    this.login = this.login.bind(this); // <-- add this line
+    this.logout = this.logout.bind(this); // <-- add this line
   }
 
+  logout() {
+    auth.signOut()
+    .then(() => {
+      this.setState({
+        user: null,
+        practices: []
+      });
+    });
+  }
 
+  login() {
+    auth.signInWithPopup(provider) 
+      .then((result) => {
+        const user = result.user;
+        this.setState({
+          user: user,
+          practices: []
+        });
+
+        user.getToken().then((token) => { 
+          this.getAllPractices(token);
+        });
+      });
+  }
+
+  // {headers.append(}'Accept', 'application/json');
+  // headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+  // headers.append("Authorization", "Bearer " + token);
+
+
+  getAllPractices(token){
+    axios.get(`http://localhost:56617/api/practice`, {
+      headers: {
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*', //'http://localhost:4200',
+        Authorization: 'Bearer ' + token
+      }
+    })
+    .then(res => {
+      const testPractice = res.data;
+      this.setState({ practices: testPractice });
+    });
+  }
 
 
   render() {
@@ -32,9 +74,17 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div>
+        <div className='container'>
+        {
+          this.state.user ?
+          <button onClick={this.logout}>Log Out</button>                
+          :
+          <button onClick={this.login}>Log In</button>              
+        }
+        </div>
           
           {this.state.practices.map((practice, index) => (
-              <div><span>{practice.Name} {practice.PracticeDateTimeStamp}</span></div>
+              <div key={practice.id}><span>{practice.name} {practice.practiceDateTimeStamp}</span></div>
           ))}
         </div>
       </div>
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyA4UDCqMKJWPorZXQ7lh59ZawOPhqSl63U",
+    authDomain: "mtarcherycoach.firebaseapp.com",
+    databaseURL: "https://mtarcherycoach.firebaseio.com",
+    projectId: "mtarcherycoach",
+    storageBucket: "mtarcherycoach.appspot.com",
+    messagingSenderId: "337400391771"
+};
+firebase.initializeApp(config);
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export default firebase;
