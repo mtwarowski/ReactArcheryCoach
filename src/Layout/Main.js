1443bdc412b4c9323b0412afff770d245261fb20
Michał Twarowski <m.twarowski1988@gmail.com>
2018-03-02 08:28:12 +0000

    Playing with images

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -4,7 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
 import Dashboard from '../Dashboard';
-import Bows from '../Equipment/Bows';
+import Bows from '../Equipment/bows';
+import Arrows from '../Equipment/Arrows';
 import PracticesPage from '../Practicing/PracticesPage';
 
 const Main = () => (
@@ -13,6 +14,7 @@ const Main = () => (
             <Route exact path='/' component={Dashboard} />
             <PrivateRoute path='/Practices' component={PracticesPage} />
             <PrivateRoute path='/Bows' component={Bows} />
+            <PrivateRoute path='/Arrows' component={Arrows} />
         </Switch>
     </main>
 )
