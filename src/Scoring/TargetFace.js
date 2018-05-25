759188b4d68dfc22009c47cc61ab8a7784d99b30
Michal.Twarowski <m.twarowski1988@gmail.com>
2018-05-25 16:02:28 +0100

    More work on targetFace

--- src/Scoring/TargetFace.js
+++ src/Scoring/TargetFace.js
@@ -1,44 +1,61 @@
 import React, { Component } from 'react';
-import { Stage, Layer, Circle, Rect, Text } from 'react-konva';
+import { Stage, Layer, Circle, Rect, Text, Group } from 'react-konva';
 import Konva from 'konva';
+
+import Avatar from 'material-ui/Avatar';
+import RaisedButton from 'material-ui/RaisedButton';
+import NavigationArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import NavigationArrowDropUp from 'material-ui/svg-icons/navigation/arrow-drop-up';
 
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import ContentRemove from 'material-ui/svg-icons/content/remove';
 import { throttle } from 'lodash';
+import { POINT_CONVERSION_HYBRID } from 'constants';
 
+class ArrowPointBar extends React.Component {
 
-
-class SizeWrapper extends React.Component {
     constructor(props) {
-        super(props)
-        this.resize = this.resize.bind(this)
+        super(props);
+        this.state = {
+            isOpened: false
+        };
     }
-
-    componentDidMount() {
-        window.addEventListener('resize', this.resize);
-        this.resize();
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.resize);
-    }
-
-    resize = throttle(() => {
-        this.width = this.div.parentNode.clientWidth;
-        this.height = this.div.parentNode.clientHeight;
-    }, 50)
-
-    setRef = node => {
-        this.div = node;
-    }
-
+    
     render() {
-        return <div className={this.props.className} ref={this.setRef}>
-            before
-            {this.props.children}
-            after
-      </div>;
+        const styles = {
+            arrowPointBarContainer: {
+                margin: 0,
+                top: 50,
+                left: 0,
+                position: 'fixed',
+                width: '100%'
+            },
+            arrowPointPresenterContainer:{
+                backgroundColor: 'white',
+                paddingTop: 10,
+
+            },
+            arrowPointPresenter: {
+                marginLeft: '5px'
+            },
+            navArrowDropUpDownButton:{
+                float: 'right',
+                marginRight: '10px'
+            }
+        };
+
+        return (
+        <div style={styles.arrowPointBarContainer}>
+            {this.state.isOpened && 
+            <div style={styles.arrowPointPresenterContainer} >
+                {this.props.points.map((arrowPoint, idx) => 
+                    <Avatar style={styles.arrowPointPresenter} onClick={() => this.props.onArrowPointSelected(arrowPoint)} key={idx}>{arrowPoint.xPos}</Avatar>
+                )}
+            </div>
+            }
+            <RaisedButton style={styles.navArrowDropUpDownButton} onClick={() => this.setState({isOpened: !this.state.isOpened})} icon={this.state.isOpened ? <NavigationArrowDropUp /> : <NavigationArrowDropDown />} />
+        </div>);
     }
 }
 
@@ -48,38 +65,40 @@ class ArrowPoint extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            isEditMode: true
-        };
 
         this.handleClick = this.handleClick.bind(this);
     }
 
+    //not sure do i need that. The point is so small it's hard to click it
     handleClick(e) {
-
-        this.setState({
-            isEditMode: true
-        });
+        this.props.handlePointChanged({...this.props.point, isEditMode: !this.props.point.isEditMode}, this.props.pointIndex);
     };
 
-    handlePointDragEnd = (e, index) => {
+    handlePointGroupDragEnd = (e) => {
         let newPoint = {
-            xPos: e.target.x(),
-            yPos: e.target.y()
+            xPos: this.props.point.xPos + e.target.x(),
+            yPos: this.props.point.yPos + e.target.y(),
+            isEditMode: !this.props.point.isEditMode
         };
 
-        this.setState({
-            isEditMode: false
-        });
-        this.props.handlePointChanged(newPoint, index);
+        this.props.handlePointChanged(newPoint, this.props.pointIndex);
+    }
+
+    handlePointDragEnd = (e) => {
+        let newPoint = {
+            xPos: e.target.x(),
+            yPos: e.target.y(),
+            isEditMode: !this.props.point.isEditMode
+        };
+
+        this.props.handlePointChanged(newPoint, this.props.pointIndex);
     }
 
     getEditModeView() {
         return (
-            <Layer>
+            <Group ref={(node) => this.groupNode = node }
+            draggable={true} onDragEnd={(e) => this.handlePointGroupDragEnd(e)}>
                 <Circle
-                    draggable={true}
-                    onDragEnd={(e) => this.handlePointDragEnd(e, this.props.pointIndex)}
                     x={this.props.point.xPos}
                     y={this.props.point.yPos}
                     radius={30}
@@ -87,6 +106,7 @@ class ArrowPoint extends React.Component {
                     fill={'rgba(255, 0, 0, 0.4)'}
                     strokeWidth={2} />
                 <Circle
+                    ref={(node) => this.pointNode = node }
                     x={this.props.point.xPos}
                     y={this.props.point.yPos}
                     radius={5}
@@ -94,12 +114,12 @@ class ArrowPoint extends React.Component {
                     stroke={'grey'}
                     strokeWidth={1}
                     onClick={this.handleClick} />
-            </Layer>);
+            </Group>);
     }
 
     getNotEditModeView() {
         return (
-            <Layer>
+            <Group>
                 <Circle
                     x={this.props.point.xPos}
                     y={this.props.point.yPos}
@@ -108,13 +128,13 @@ class ArrowPoint extends React.Component {
                     stroke={'grey'}
                     strokeWidth={1}
                     onClick={this.handleClick} />
-            </Layer>);
+            </Group>);
     }
 
     render() {
         return (
             this.props.point ?
-                this.state.isEditMode ?
+                this.props.point.isEditMode ?
                     this.getEditModeView()
                     : this.getNotEditModeView()
                 : ''
@@ -129,8 +149,8 @@ export default class TargetFace extends Component {
         this.state = {
             targetFace: this.getTargetFace(),
             points: [{ xPos: 100, yPos: 100 }, { xPos: 200, yPos: 100 }],
-            width: 500,
-            height: 500,
+            width: 50,
+            height: 50,
             scale: {
                 x: 1,
                 y: 1
@@ -138,10 +158,11 @@ export default class TargetFace extends Component {
         };
 
         this.scaleStage = this.scaleStage.bind(this);
-        this.addNewPoint = this.addNewPoint.bind(this);
+        this.handleStageElementClicked = this.handleStageElementClicked.bind(this);
         this.handlePointChanged = this.handlePointChanged.bind(this);
 
-
+        this.handleArrowPointBarSelected = this.handleArrowPointBarSelected.bind(this);
+        this.handleAddNewArrowPoint = this.handleAddNewArrowPoint.bind(this);
         this.resize = this.resize.bind(this);
     }
 
@@ -149,7 +170,7 @@ export default class TargetFace extends Component {
         return {
             paperSize: {
                 width: 1000,
-                height: 1000,
+                height: 2500,
             },
             targets: [
                 [
@@ -182,17 +203,12 @@ export default class TargetFace extends Component {
         };
     }
 
-    addNewPoint(e) {
+    addNewArrowPoint(xPos, yPos){        
         this.state.points.push({
-            xPos: e.target.x(),
-            yPos: e.target.y()
+            xPos: xPos,
+            yPos: yPos
         });
 
-        this.setState({ points: this.state.points });
-    }
-
-    handlePointChanged(point, index) {
-        this.state.points[index] = point;
         this.setState({ points: this.state.points });
     }
 
@@ -203,63 +219,97 @@ export default class TargetFace extends Component {
 
 
     componentDidMount() {
-        window.addEventListener('resize', this.resize);
         this.resize();
-    }
-
+        window.addEventListener('resize', this.resize);
+      }
+      
     componentWillUnmount() {
         window.removeEventListener('resize', this.resize);
     }
-
-    resize = throttle(() => {
-
-        this.setState({
-            width: this.div.parentNode.clientWidth,
-            height: this.div.parentNode.clientHeight
-        });
-
-        this.width = this.div.parentNode.clientWidth;
-        this.height = this.div.parentNode.clientHeight;
-    }, 50)
-
-    setRef = node => {
-        this.div = node;
+        
+    resize() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight - 80 });
     }
 
+
+    handleArrowPointBarSelected(point){
+        point.isEditMode = !point.isEditMode;
+        this.setState({points: this.state.points}); 
+    }
+
+    handleAddNewArrowPoint(){
+        this.addNewArrowPoint(0, 0);
+    }
+
+    handleStageElementClicked(e) {
+        this.addNewArrowPoint(e.target.x(), e.target.y());
+    }
+    
+    handlePointChanged(point, index) {
+        this.state.points[index] = point;
+        this.setState({ points: this.state.points });
+    }
+
+
     render() {
+
+        const styles = {            
+            margin: 0,
+            top: 'auto',
+            right: 20,
+            bottom: 20,
+            left: 'auto',
+            position: 'fixed'
+        }
         return (
-            <div style={{ width: this.state.height, width: this.state.width }} ref={this.setRef}>
-                <div>
+
+            <div>
+                <Stage draggable={true}
+                    onClick={this.handleStageElementClicked}
+                    scale={this.state.scale}
+                    height={this.state.height}
+                    width={this.state.width}>
+                    <Layer>
+                    <Group>
+                        <Rect
+                            x={-10}
+                            y={-10}
+                            width={this.state.targetFace.paperSize.width + 20}
+                            height={this.state.targetFace.paperSize.height + 20}
+                            fill={'white'}
+                            shadowBlur={20}
+                        />
+                    </Group>
+                    {this.state.targetFace.targets.map(((target, index) =>
+                        <Group key={index}>
+                            {target.map((targetRing, idx) =>
+                                <Circle key={idx}
+                                    x={targetRing.xPos}
+                                    y={targetRing.yPos}
+                                    onClick={this.handleStageElementClicked}
+                                    radius={targetRing.radius}
+                                    fill={targetRing.backgroundColor}
+                                    stroke={targetRing.lineColour}
+                                    strokeWidth={targetRing.lineWidth} />
+                            )}
+                        </Group>
+                    ))}
+                    {this.state.points.map((point, index) => <ArrowPoint key={index} point={point} pointIndex={index} handlePointChanged={this.handlePointChanged} />)}
+
+                    </Layer>
+                </Stage>
+                <div style={styles}>
                     <FloatingActionButton mini={true} onClick={() => this.scaleStage(-0.1)}>
                         <ContentRemove />
                     </FloatingActionButton>
                     <FloatingActionButton mini={true} onClick={() => this.scaleStage(0.1)}>
                         <ContentAdd />
                     </FloatingActionButton>
+                    <FloatingActionButton secondary={true} mini={true} onClick={this.handleAddNewArrowPoint}>
+                        <ContentAdd />
+                    </FloatingActionButton>
                 </div>
-                <Stage draggable={true}
-                    onClick={this.addNewPoint}
-                    scale={this.state.scale}
-                    height={this.state.height}
-                    width={this.state.width}>
-                    {this.state.targetFace.targets.map(((target, index) =>
-                        <Layer key={index}>
-                            {target.map((targetRing, idx) =>
-                                <Circle key={idx}
-                                    x={targetRing.xPos}
-                                    y={targetRing.yPos}
-                                    onClick={this.addNewPoint}
-                                    radius={targetRing.radius}
-                                    fill={targetRing.backgroundColor}
-                                    stroke={targetRing.lineColour}
-                                    strokeWidth={targetRing.lineWidth} />
-                            )}
-                        </Layer>
-                    ))}
-                    {this.state.points.map((point, index) => <ArrowPoint key={index} point={point} pointIndex={index} handlePointChanged={this.handlePointChanged} />)}
-                </Stage>
-
-
+                <ArrowPointBar points={this.state.points} onArrowPointSelected={this.handleArrowPointBarSelected} />
             </div>
         );
     }
