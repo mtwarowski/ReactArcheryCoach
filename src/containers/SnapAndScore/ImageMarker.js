b43783e31879427a3aec0ef1700ab52e5f7be1d0
Your Name <firstname.lastname@just-eat.com>
2018-10-04 16:08:47 +0100

    Missing method

--- src/containers/SnapAndScore/ImageMarker.js
+++ src/containers/SnapAndScore/ImageMarker.js
@@ -99,6 +99,10 @@ class ImageMarker extends Component {
     }
   }
 
+  getDistance(p1, p2) {
+      return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
+
   handleOnTouchEnd() {
     this.setState({ lastDist: 0 });
   }
